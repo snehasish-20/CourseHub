b90738a38a235589b8ce8f4b12235b47d08111f6
Snehasish Pati <63896969+snehasish-20@users.noreply.github.com>
2021-07-24 22:49:46 +0530

    updated menu.js

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,16 +1,33 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import { ListGroup } from "reactstrap";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import { ListGroup, Row, Col } from "reactstrap";
+import AddCourse from "./AddCourse";
+import AllCourses from "./AllCourses";
+import Home from "./Home";
+
 
 export const Menus = ()=>{
     return(
-        <ListGroup>
-            <Link tag="a" className="list-group-item" to="/">Home</Link>
-            <Link tag="a" className="list-group-item" to="/all-courses">All Courses</Link>
-            <Link tag="a" className="list-group-item" to="/add-course">Add Course</Link>
-            <Link tag="a" className="list-group-item" to="#!">Update Course</Link>
-            <Link tag="a" className="list-group-item" to="#!">About Us</Link>
-            <Link tag="a" className="list-group-item" to="#!">Contact Us</Link>
-        </ListGroup>
+        <Row>
+                <Router>
+                    <Col md={3}>
+                        <ListGroup>
+                        <Link tag="a" className="list-group-item" to="/">Home</Link>
+                        <Link tag="a" className="list-group-item" to="/all-courses">All Courses</Link>
+                        <Link tag="a" className="list-group-item" to="/add-course">Add Course</Link>
+                        <Link tag="a" className="list-group-item" to="#!">Update Course</Link>
+                        <Link tag="a" className="list-group-item" to="#!">About Us</Link>
+                        <Link tag="a" className="list-group-item" to="#!">Contact Us</Link>
+                        </ListGroup>
+                    </Col>
+                    <Col md={9}>
+                        <Switch>
+                        <Route path="/" component={Home} exact />
+                        <Route path="/add-course" component={AddCourse}/>
+                        <Route path="/all-courses" component={AllCourses}/>
+                        </Switch>
+                    </Col>
+                    </Router>
+                </Row>
     )
 }
