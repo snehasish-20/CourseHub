303560bbdb34e93393741c2de1ff042741e3273e
Snehasish Pati <63896969+snehasish-20@users.noreply.github.com>
2021-07-24 22:48:49 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React from "react";
 import {Header} from "./components/Header";
-// import {Menus} from "./components/Menus";
-import { Container, Row, Col } from "reactstrap";
-import Home from "./components/Home";
-import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-import AddCourse from "./components/AddCourse";
-import AllCourses from "./components/AllCourses";
-import { ListGroup } from "reactstrap";
+import {Menus} from "./components/Menus";
+import { Container, } from "reactstrap";
 
+// import {BrowserRouter as Router} from "react-router-dom";
+// import AddCourse from "./components/AddCourse";
+// import AllCourses from "./components/AllCourses";
+// import { ListGroup } from "reactstrap";
+// import Home from "./components/Home";
 export default function App(){
     return(
-        <Router>
             <Container fluid="true">
             <Header/>
-                <Row>
-                    <Col md={3}>
-                    <ListGroup>
-            <Link tag="a" className="list-group-item" to="/">Home</Link>
-            <Link tag="a" className="list-group-item" to="/all-courses">All Courses</Link>
-            <Link tag="a" className="list-group-item" to="/add-course">Add Course</Link>
-            <Link tag="a" className="list-group-item" to="#!">Update Course</Link>
-            <Link tag="a" className="list-group-item" to="#!">About Us</Link>
-            <Link tag="a" className="list-group-item" to="#!">Contact Us</Link>
-        </ListGroup>
-                    </Col>
-                    <Col md={9}>
-                        <Switch>
-                        <Route path="/" component={Home} exact />
-                        <Route path="add-course" component={AddCourse}/>
-                        <Route path="all-courses" component={AllCourses}/>
-                        </Switch>
-                    </Col>
-                </Row>
+            <Menus/>
             </Container>
-        </Router>
     )
 }
